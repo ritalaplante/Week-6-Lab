3cb638590147b782b74d81e8e0a709782606fc9a
ritalaplante <45489428+ritalaplante@users.noreply.github.com>
2021-03-01 12:55:28 -0500

    Add files via upload

---
+++ Lab Week 6.py
@@ -0,0 +1,37 @@
+#Part 1: Nested For loops
+# 1. Print a triangle of numbers: ask the user for an numerical input and then
+# print out a triangle as follows: if the user input is 5 you should print:
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+
+# 2. Write a similar function, but instead print out the exponents. i.e if user
+# input is 5 you want to print
+# 1
+# 2 4
+# 3 9 27
+# 4 16 64 256
+# 5 25 125 625 3125
+
+#3. Print week and day. Ask user to input a number of weeks and a number of days.
+# for each week you want to print the days below it. For example: If weeks = 2 and
+# days = 3 you want to print
+# Week 1:
+    # Day 1
+    # Day 2
+    # Day 3
+# Week 2:
+    # Day 1
+    # Day 2
+    # Day 3
+
+#Part 2: return values
+# 1. Write a function that calculates the perimeter of a rectangle. This function
+# should take in two side lenghts as arguments and return the perimeter value.
+
+# Write a function that reverses a string. Instead of printing the reversed string
+# the function should return the reversed string which is printed in the main
+# function. 
+
